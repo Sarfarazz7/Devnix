f1f9d59b4faea354231e0e39a8a838308e7efb95
Sarfaraz Sheikh <sheikhsarfaraz1912@gmail.com>
2026-05-20 23:00:43 +0530

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -257,8 +257,6 @@ function ensureFinMonthSelector() {
         const wrap = document.createElement('div');
         wrap.className = 'fin-month-wrap';
         wrap.innerHTML = ` 
-            <span class="fin-month-label">VIEWING:</span> 
-            <input type="month" id="finMonthPick" value="${S.finMonth}"/> 
             <span class="fin-month-label">
                 <svg viewBox="0 0 24 24"><path d="M19 4h-1V2h-2v2H8V2H6v2H5c-1.11 0-1.99.9-1.99 2L3 20c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 16H5V9h14v11z"/></svg>
                 VIEWING:
